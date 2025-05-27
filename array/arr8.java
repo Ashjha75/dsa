8ec88962e7969d30f6768aa2ee722d4209a151f1
Ashish jha <ajha5645@gmail.com>
2025-05-27 14:12:12 +0530

    add arr8 class to find the missing number in an array

--- array/arr8.java
+++ array/arr8.java
@@ -1,4 +1,21 @@
 package array;
 
+//find missing number
 public class arr8 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 0, 3, 6, 4};
+
+        System.out.println(missingNumber(arr));
+
+    }
+
+    public static int missingNumber(int[] nums) {
+        int N = nums.length;
+        int totalSum = N * (N + 1) / 2;
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += nums[i];
+        }
+        return (totalSum - sum);
+    }
 }
