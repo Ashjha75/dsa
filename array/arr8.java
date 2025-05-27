eaa8109acf58c7f61673bd1ecc96a16ae462a23b
Ashish jha <ajha5645@gmail.com>
2025-05-27 14:36:51 +0530

    add missingNumber2 method to arr8 class and refactor missingNumber implementation

--- array/arr8.java
+++ array/arr8.java
@@ -1,21 +1,37 @@
 package array;
 
-//find missing number
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class arr8 {
     public static void main(String[] args) {
         int[] arr = {1, 2, 0, 3, 6, 4};
-
         System.out.println(missingNumber(arr));
-
     }
 
     public static int missingNumber(int[] nums) {
-        int N = nums.length;
-        int totalSum = N * (N + 1) / 2;
-        int sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += nums[i];
+        ArrayList<Integer> set = new ArrayList<>();
+        for (int num : nums) {
+            set.add(num);
         }
-        return (totalSum - sum);
+        int N = nums.length;
+        for (int i = 0; i <= N; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+        return -1; // Should not reach here if input is valid
     }
+
+//    using sum
+public int missingNumber2(int[] nums) {
+    int N =nums.length;
+    int totalSum=N*(N+1)/2;
+    int sum=0;
+    for(int i=0;i<N;i++){
+        totalSum-=nums[i];
+    }
+    return (totalSum);
+}
+
 }
