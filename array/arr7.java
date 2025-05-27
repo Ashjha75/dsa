ea6c82418817ff8c0d489e203ebdb1e7007e8b22
Ashish jha <ajha5645@gmail.com>
2025-05-27 12:22:11 +0530

    add arr7 class to create a union of two sorted arrays

--- array/arr7.java
+++ array/arr7.java
@@ -1,4 +1,62 @@
+// file: `array/arr7.java`
 package array;
 
+import java.util.ArrayList;
+
 public class arr7 {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr2 = {2, 3, 4, 4, 5, 11, 12};
+
+        // dono sorted array ka union banane ke liye function call
+        ArrayList<Integer> arrUnion = makeUnionOfArray(arr1, arr2);
+        for (int num : arrUnion) {
+            System.out.println(num);
+        }
+    }
+
+    // makeUnionOfArray function do pointer technique ka use karta hai
+    private static ArrayList<Integer> makeUnionOfArray(int[] arr1, int[] arr2) {
+        ArrayList<Integer> unionArray = new ArrayList<>();
+        int p1 = 0;
+        int p2 = 0;
+
+        // jab tak dono arrays me elements bache hai tab tak compare karo
+        while (p1 < arr1.length && p2 < arr2.length) {
+            if (arr1[p1] < arr2[p2]) {
+                if (!unionArray.contains(arr1[p1])) {
+                    unionArray.add(arr1[p1]);
+                }
+                p1++;
+            } else if (arr1[p1] > arr2[p2]) {
+                if (!unionArray.contains(arr2[p2])) {
+                    unionArray.add(arr2[p2]);
+                }
+                p2++;
+            } else {
+                // dono element equal hain, sirf ek baar add karo
+                if (!unionArray.contains(arr1[p1])) {
+                    unionArray.add(arr1[p1]);
+                }
+                p1++;
+                p2++;
+            }
+        }
+
+        // agar arr1 me bache elements hain to add karo
+        while (p1 < arr1.length) {
+            if (!unionArray.contains(arr1[p1])) {
+                unionArray.add(arr1[p1]);
+            }
+            p1++;
+        }
+        // agar arr2 me bache elements hain to add karo
+        while (p2 < arr2.length) {
+            if (!unionArray.contains(arr2[p2])) {
+                unionArray.add(arr2[p2]);
+            }
+            p2++;
+        }
+        return unionArray;
+    }
 }
