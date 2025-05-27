612a49579aa7c3693eefa149ac7ced2035fb0848
Ashish jha <ajha5645@gmail.com>
2025-05-27 15:16:46 +0530

    add missingNumber2 method to arr8 class and refactor missingNumber implementation

---
+++ array/arr9.java
@@ -0,0 +1,25 @@
+package array;
+
+public class arr9 {
+    public static void main(String[] args) {
+        int[] arr = {0,0,0,0,0,0,0,1};
+        int maxConsecutiveCount = findMaxConsecutiveOnes(arr);
+        System.out.println(maxConsecutiveCount);
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxConsecutiveCount = 0;
+        int consecutiveCount = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                consecutiveCount++;
+                if (consecutiveCount > maxConsecutiveCount) {
+                    maxConsecutiveCount = consecutiveCount;
+                }
+            } else {
+                consecutiveCount = 0;
+            }
+        }
+        return maxConsecutiveCount;
+    }
+}
