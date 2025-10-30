629a36cda715febe099fa5f5dad89c4fc897aaef
Ashish jha <ajha5645@gmail.com>
2025-10-30 19:59:26 +0530

    add: implement custom search method and main entry point in basics class; add .gitignore and XML configuration files

--- array/basics.java
+++ array/basics.java
@@ -1,4 +1,30 @@
 package array;
 
 public class basics {
+
+    public static void main(String[] args) {
+        int[] array={9,1,4,11,6,2,7,8,9,5};
+//        call customSearch
+        int index=customSearch(array,111);
+        System.out.println(index);
+    }
+
+
+//    simple custom search
+    public  static  int customSearch(int[] array,int searchValue){
+        for(int i=0;i<array.length;i++){
+            if(array[i]==searchValue){
+                return i;
+            }
+
+        }
+        return  -1;
+    }
+
+//    insertion
+    public  static int insertion(int[] array,int value){
+
+        
+
+    }
 }
